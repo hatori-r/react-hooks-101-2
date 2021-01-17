a79a75df374b1f310ab48dc449e88c17678ad7b2
hatori-r <aruue368@gmail.com>
2021-01-17 18:17:15 +0900

    display events, refactor and implement remove event button

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { useReducer, useState } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Event from './Event'
+
 import reducer from '../reducers';
 
 const App = () => {
@@ -21,8 +23,6 @@ const App = () => {
     setTitle('')
     setBody('')
   }
-
-  console.log({state})
 
   return (
     <div className="container-fluid">
@@ -53,7 +53,7 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-
+          { state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />)) }
         </tbody>
       </table>
     </div>
